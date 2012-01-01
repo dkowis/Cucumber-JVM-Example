a814abc8d3006dd36f6c6613c2dbc7592d2def2f
David Kowis <dkowis@shlrm.org>
2012-01-01 15:49:12 -0600

    Implemented most of the project

--- src/main/java/com/rackspace/example/stack/RackStack.java
+++ src/main/java/com/rackspace/example/stack/RackStack.java
@@ -1,4 +1,19 @@
 package com.rackspace.example.stack;
 
+import java.util.Stack;
+
 public class RackStack {
+    private Stack stack = new Stack();
+    
+    public void push(Object o) {
+        stack.push(o);
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public Object pop() {
+        return stack.pop();
+    }
 }
--- src/test/java/com/rackspace/example/stack/StackStepDefs.java
+++ src/test/java/com/rackspace/example/stack/StackStepDefs.java
@@ -7,14 +7,18 @@ import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
 
+import static junit.framework.Assert.assertEquals;
+
 public class StackStepDefs {
     
-    private RackStack myStack = new RackStack();
+    private RackStack myStack;
+    private Object pushed;
+    private Object popped;
 
     @When("^I pop from the stack$")
-    @Pending
     public void I_pop_from_the_stack() {
-        // Express the Regexp above with the code you wish you had
+        Object out = myStack.pop();
+        popped = out;
     }
 
     @When("^I push another item into the stack$")
@@ -24,21 +28,18 @@ public class StackStepDefs {
     }
 
     @Given("^an empty stack$")
-    @Pending
     public void an_empty_stack() {
-        // Express the Regexp above with the code you wish you had
+        myStack = new RackStack();
     }
 
     @Then("^I get the same item back$")
-    @Pending
     public void I_get_the_same_item_back() {
-        // Express the Regexp above with the code you wish you had
+        assertEquals(pushed, popped);
     }
 
     @Then("^the stack contains one item$")
-    @Pending
     public void the_stack_contains_one_item() {
-        // Express the Regexp above with the code you wish you had
+        assertEquals(1, myStack.size());
     }
 
     @Then("^the stack contains two items$")
@@ -48,9 +49,9 @@ public class StackStepDefs {
     }
 
     @When("^I push an item into the stack$")
-    @Pending
     public void I_push_an_item_into_the_stack() {
-        // Express the Regexp above with the code you wish you had
+        pushed = new Object();
+        myStack.push(pushed);
     }
 
 }
