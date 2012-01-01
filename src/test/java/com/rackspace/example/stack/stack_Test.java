2f96eeaaf20ea64d6a4fd4491c2c43e48e8209cb
David Kowis <dkowis@shlrm.org>
2012-01-01 15:35:04 -0600

    Adding initial feature and feature runner

---
+++ src/test/java/com/rackspace/example/stack/stack_Test.java
@@ -0,0 +1,11 @@
+package com.rackspace.example.stack;
+
+import cucumber.junit.Cucumber;
+import cucumber.junit.Feature;
+import org.junit.runner.RunWith;
+
+@RunWith(value = Cucumber.class)
+@Feature(value = "stack.feature")
+public class stack_Test {
+
+}
