2e742585976e1df48905118ec896ad4e5fc3a072
David Kowis <david.kowis@rackspace.com>
2012-01-03 14:58:25 -0600

    Adding a scenario using a table for data entry

---
+++ src/test/java/com/rackspace/example/stack/step_definitions/TableTimeSteps.java
@@ -0,0 +1,36 @@
+package com.rackspace.example.stack.step_definitions;
+
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+import cucumber.table.DataTable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TableTimeSteps {
+    
+    @Autowired
+    private NinjaContext context;
+
+    @Then("^the stack contains \"([^\"]*)\" items$")
+    public void the_stack_contains_items(int arg2) {
+        assertEquals(arg2, context.getMyStack().size());
+    }
+
+    @When("^I push many things into the stack:$")
+    public void I_push_many_things_into_the_stack(DataTable table) {
+
+        //This is returning a list of nulls right now!
+        //List<String> things = table.asList(String.class);
+        
+        for(List<String> rows : table.raw()){
+            for(String entry: rows){
+                context.getMyStack().push(entry);
+            }
+        }
+        
+    }
+
+}
