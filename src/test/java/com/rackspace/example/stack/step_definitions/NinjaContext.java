a8e3499a2b04b56c7cd9ac6f6e64568f346675fc
David Kowis <david.kowis@rackspace.com>
2012-01-03 11:56:10 -0600

    set up enough to fire up spring injection of stuff

---
+++ src/test/java/com/rackspace/example/stack/step_definitions/NinjaContext.java
@@ -0,0 +1,35 @@
+package com.rackspace.example.stack.step_definitions;
+
+import com.rackspace.example.stack.RackStack;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NinjaContext {
+    private RackStack myStack;
+    private Object pushed;
+    private Object popped;
+
+    public Object getPushed() {
+        return pushed;
+    }
+
+    public void setPushed(Object pushed) {
+        this.pushed = pushed;
+    }
+
+    public Object getPopped() {
+        return popped;
+    }
+
+    public void setPopped(Object popped) {
+        this.popped = popped;
+    }
+
+    public RackStack getMyStack() {
+        return myStack;
+    }
+
+    public void setMyStack(RackStack myStack) {
+        this.myStack = myStack;
+    }
+}
--- src/test/java/com/rackspace/example/stack/step_definitions/StackStepDefs.java
+++ src/test/java/com/rackspace/example/stack/step_definitions/StackStepDefs.java
@@ -1,24 +1,27 @@
-package com.rackspace.example.stack;
+package com.rackspace.example.stack.step_definitions;
 
 
+import com.rackspace.example.stack.RackStack;
 import cucumber.annotation.Pending;
 import cucumber.annotation.en.And;
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import static junit.framework.Assert.assertEquals;
 
+@Component
 public class StackStepDefs {
     
-    private RackStack myStack;
-    private Object pushed;
-    private Object popped;
-
+    @Autowired
+    private NinjaContext context;
+    
     @When("^I pop from the stack$")
     public void I_pop_from_the_stack() {
-        Object out = myStack.pop();
-        popped = out;
+        Object out = context.getMyStack().pop();
+        context.setPopped(out);
     }
 
     @When("^I push another item into the stack$")
@@ -28,28 +31,29 @@ public class StackStepDefs {
 
     @Given("^an empty stack$")
     public void an_empty_stack() {
-        myStack = new RackStack();
+
+        context.setMyStack(new RackStack());
     }
 
     @Then("^I get the same item back$")
     public void I_get_the_same_item_back() {
-        assertEquals(pushed, popped);
+        assertEquals(context.getPushed(), context.getPopped());
     }
 
     @Then("^the stack contains one item$")
     public void the_stack_contains_one_item() {
-        assertEquals(1, myStack.size());
+        assertEquals(1, context.getMyStack().size());
     }
 
     @Then("^the stack contains two items$")
     public void the_stack_contains_two_items() {
-        assertEquals(2, myStack.size());
+        assertEquals(2, context.getMyStack().size());
     }
 
     @When("^I push an item into the stack$")
     public void I_push_an_item_into_the_stack() {
-        pushed = new Object();
-        myStack.push(pushed);
+        context.setPushed(new Object());
+        context.getMyStack().push(context.getPushed());
     }
 
 }
